b968a26ad118044a460701c02242c159b8e49c38
UmbraCharon <ship154@gmail.com>
2016-05-02 22:54:30 -0400

    42

--- programs/nate/satan.js
+++ programs/nate/satan.js
@@ -1,10 +1,10 @@
-
+        
 new explorer.window().callback(function() {
     dis = this;
     bod = this.body;
     bod.html('<button class="asdf hi">1</button>');
     bod.find('.asdf').click(function() {
-        var count = parseInt($(this).text()) + 1;
+        var count = parseInt($(this).text()) + 42;
         $(this).text(count);
         dis.title(count);
     }).attr('style', 'width:100px;height:100px;background-image:url("programs/nate/1.png");background-size:100%;');
