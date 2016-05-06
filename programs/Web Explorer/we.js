c610f114601ad38496d35c0c3cf8fdca1f1b63b6
Kristopher <dylan@belowaverage.org>
2016-05-05 21:39:24 -0400

    Fixed it lol

--- programs/Web Explorer/we.js
+++ programs/Web Explorer/we.js
@@ -21,7 +21,7 @@ new explorer.window()
     //Function to change the iframe to the givin URL.
     function changeUrl(url) {
         //Change the iframe
-        bod.find('iframe.i1').val(url);
+        bod.find('input.i1').val(url);
         
         //Change the window title
         win.title('Web Explorer - '+url)
@@ -33,22 +33,22 @@ new explorer.window()
     //Give the button its position via the style (CSS) and piggyback a click event if it is clicked.
     bod.find('button.b1').attr('style', 'position:absolute;left:1px;top:1px;width:24px;height:21px;').click(function() {
         //If button is clicked find the iframe and go back 1 page in its history.
-        bod.find('.if1')[0].contentWindow.history.go(-1);
+        bod.find('iframe.if1')[0].contentWindow.history.go(-1);
     });
     
     //Give the button its position via the style (CSS) and piggyback a click event if it is clicked.
     bod.find('button.b2').attr('style', 'position:absolute;left:24px;top:1px;width:24px;height:21px;').click(function() {
         //If button is clicked find the iframe and go forward 1 page in its history.
-        bod.find('.if1')[0].contentWindow.history.go(1);
+        bod.find('iframe.if1')[0].contentWindow.history.go(1);
     });
     
     //Give the third button its position and click event.
     bod.find('button.b3').attr('style', 'position:absolute;right:1px;top:1px;').click(function() {
         //On click get the text inside the input form
-        var url = bod.find('.i1').val();
+        var url = bod.find('input.i1').val();
         
         //Put the text (URL) into the iframe
-        bod.find('.if1').attr('src', url);
+        bod.find('iframe.if1').attr('src', url);
         
         //Start the tracker service in case it has stopped in the past.
         startTracker();
@@ -82,7 +82,7 @@ new explorer.window()
             
             try {
                 //try to set url1 to the actual current url
-                url1 = bod.find('.if1')[0].contentDocument.URL;
+                url1 = bod.find('iframe.if1')[0].contentDocument.URL;
             } catch(e) {
                 //if failed to get url stop the interval
                 clearInterval(interval);
