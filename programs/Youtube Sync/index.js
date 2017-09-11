610ffc6f467a1c8f7990d8987501ded1f1636dee
krisdb2009 <dylan@belowaverage.org>
2017-09-11 19:00:33 -0400

    Youtube Sync Add Player

--- programs/Youtube Sync/index.js
+++ programs/Youtube Sync/index.js
@@ -4,6 +4,7 @@ var main = new explorer.window()
 .resize(500, 360)
 .center('', -100, -100);
 var sat = {};
+var player = {};
 function openSat() {
 	sat = new explorer.window()
 	.title('Youtube Sync - Queue')
@@ -24,6 +25,21 @@ main.mBar = [
 				callback: function() {
 					main.close();
 				}
+			}, {
+				title: 'Test',
+				callback: function() {
+					player.source({
+						type: 'video',
+						sources: [{
+							src: 'E1iwrfgNOUU',
+							type: 'youtube'
+						}]
+					});
+					player.on('ready', function() {
+						player.play();
+						console.log('asdf');
+					});
+				}
 			}
 		]
 	}, {
@@ -32,7 +48,7 @@ main.mBar = [
 			{
 				title: 'Fullscreen',
 				callback: function() {
-					video.webkitEnterFullscreen();
+					player.toggleFullscreen();
 				}
 			}, {}, {
 				title: 'Show queue...',
@@ -58,10 +74,11 @@ main.mBar = [
 main.menuBar(main.mBar);
 main.body.html(`
 	<style>
-		.window[windowid=`+main.id+`] video {
+		.window[windowid=`+main.id+`] iframe {
 			width:100%;
 			height:100%;
 			position:absolute;
+			border:0px;
 		}
 		.window[windowid=`+main.id+`] .body {
 			background-image:url('programs/Youtube Sync/icon.png');
@@ -72,6 +89,11 @@ main.body.html(`
 		}
 	</style>
 `);
-var video = $('<video controls></video>').appendTo(main.body)[0];
-//video.src = 'https://r19---sn-p5qlsnes.googlevideo.com/videoplayback?ei=jdugWee_HZeJYPnKtJAN&itag=22&ratebypass=yes&pl=32&signature=10CF1BA2867BFFDDB850BE51FAF1F980677B1F4A.98DF7F7F03037CA74F8C164AE85C091FC6F50699&mime=video%2Fmp4&key=yt6&source=youtube&initcwndbps=1097500&requiressl=yes&expire=1503735789&id=o-AMB7_U_I56yOaqUdUXkgFW_VfOGRCBL_65a5HCoo86gK&ipbits=0&mm=31&mn=sn-p5qlsnes&dur=188.592&ms=au&mt=1503714047&mv=m&sparams=dur%2Cei%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cexpire&lmt=1483593667503233&ip=2607%3Afcc8%3A6805%3A8b01%3A%3A1%3A0';
-window.vid = video;
+main.body.find('*:not(style)').remove();
+var video = $('<iframe src="programs/Youtube Sync/plyr.html"></iframe>').appendTo(main.body);
+video[0].allowFullscreen = true;
+video[0].contentWindow.onload = function() {
+	player = video[0].contentWindow.player;
+	window.player = video[0].contentWindow.player;
+	callback.call();
+};
