f5a31eb04a96a9d56d5a56583eb8f21f553c5f89
Dylan Bickerstaff <dylan@belowaverage.org>
2017-11-26 15:55:59 -0500

    Fix PIWIK

--- webdows/belowaverage/system/piwik.js
+++ webdows/belowaverage/system/piwik.js
@@ -2,7 +2,7 @@ var _paq = _paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-	var u="//api.belowaverage.org/v3/piwik/";
+	var u="https://api.belowaverage.org/v3/piwik/";
 	_paq.push(['setTrackerUrl', u+'piwik.php']);
 	_paq.push(['setSiteId', '1']);
 	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
