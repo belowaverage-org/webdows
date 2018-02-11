be1073e2b92ed9c2a20b9b814cac76d46bdbafee
Dylan K. Bickerstaff <dylan@belowaverage.org>
2018-02-11 14:49:04 -0500

    Working tracing in paint.js

--- webdows/paint.js
+++ webdows/paint.js
@@ -1,44 +1,73 @@
+var newImage = new Image();
+var oldImage = newImage;
+$(newImage).ready(function() {
+	
+});
 var win = new explorer.window()
 .title('Paint')
 .resize(800, 500)
 .center();
 win.on.close = function() {
-	clearInterval(resizeInterval);
+	clearInterval(mainInterval);
 };
 var bod = win.body;
-var canvas = $('<canvas style="width:100%;height:100%;top:0px;left:0px;position:absolute;"></canvas>').appendTo(win.body)[0];
-var resizeInterval = 0;
+var canvas = $('<canvas style="width:100%;height:100%;top:0px;left:0px;position:absolute;" width="1000" height="1000"></canvas>').appendTo(win.body)[0];
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'rgb(0, 0, 0)';
+var mainInterval = 0;
+var lastWidth = 0;
+var lastHeight = 0;
+var mouseDown = false;
+var lastTracePointX = 0;
+var lastTracePointY = 0;
+var currentTracePointX = 0;
+var currentTracePointY = 0;
 window.canvas = canvas;
 window.ctx = ctx;
 window.bod = bod;
-
+function save() {
+	newImage.src = canvas.toDataURL("image/png");
+};
+function restore() {
+	ctx.drawImage(newImage, 0, 0);
+};
 bod[0].onresize = function() {
-	console.log('asdf');
 	canvas.width = bod.width();
 	canvas.height = bod.height();
 };
-var lastWidth = 0;
-var lastHeight = 0;
-resizeInterval = setInterval(function() {
+mainInterval = setInterval(function() {
 	var width = bod.outerWidth();
-	if(width !== lastWidth) {
-		lastWidth = width;
-		canvas.width = width;
-	}
 	var height = bod.outerHeight();
-	if(height !== lastHeight) {
+	if(width !== lastWidth || height !== lastHeight) {
+		lastWidth = width;
 		lastHeight = height;
-		canvas.height = height;
+		save();
+		setTimeout(function() {
+			canvas.width = width;
+			canvas.height = height;
+			restore();
+		});
 	}
-	if(height !== lastHeight || width !== lastWidth) {
-		ctx.restore();
-	}	
-}, 60);
-
-var ctx = canvas.getContext('2d');
-ctx.fillStyle = 'rgb(0, 0, 0)';
-ctx.beginPath();
-ctx.lineTo(100, 100);
-ctx.lineTo(200, 200);
-ctx.stroke();
-ctx.save();
+	if(mouseDown) {
+		ctx.beginPath();
+		ctx.lineTo(lastTracePointX, lastTracePointY);
+		ctx.lineTo(currentTracePointX, currentTracePointY);
+		ctx.stroke();
+		lastTracePointX = currentTracePointX;
+		lastTracePointY = currentTracePointY;
+	}
+}, 16.666);
+function mouseUp(e) {
+	mouseDown = false;
+	$('#desktop').off('mouseup', mouseUp);
+};
+$(canvas).on('mousedown', function(e) {
+	$('#desktop').on('mouseup', mouseUp);
+	lastTracePointX = e.pageX - $(e.target).offset().left;
+	lastTracePointY = e.pageY - $(e.target).offset().top;
+	mouseDown = true;
+});
+$(canvas).on('mousemove', function(e) {
+	currentTracePointX = e.pageX - $(e.target).offset().left;
+	currentTracePointY = e.pageY - $(e.target).offset().top;
+});
